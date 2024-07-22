a882b13983444674c7b7741f001a54e4ea978e54
RobertKochanski <kochanskirobert1999@gmail.com>
2024-07-22 12:51:48 +0200

    Configure local IDE

---
+++ DesignPatternsIntro/Person.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatternsIntro
+{
+    internal class Person //(string name)  possible way to create contructor since .NET 8
+    {
+        public string FirstName;
+        public string LastName;
+
+        public Person(string firstname, string lastname) // older way to create contructor
+        {
+            FirstName = firstname;
+            LastName = lastname;
+        }
+    }
+}
---
+++ DesignPatternsIntro/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace DesignPatternsIntro
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person = new Person("first", "last");
+
+            Console.WriteLine(person.FirstName);
+            Console.WriteLine(person.LastName);
+        }
+    }
+}
