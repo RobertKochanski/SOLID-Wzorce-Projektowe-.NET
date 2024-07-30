2686531e6f7a31fdc9b5d3e93b455bb30d86a418
RobertKochanski <kochanskirobert1999@gmail.com>
2024-07-30 12:06:47 +0200

    Observer

---
+++ BehavioralDesignPatterns/Observer/Definition.md
@@ -0,0 +1,2 @@
+# Observer (Obserwator) - 
+### Behawioralny wzorzec projektowy pozwalaj¹cy utworzyæ mechanizm subskrypcji w celu powiadamiania wielu obiektów o zdarzeniach dziej¹cych siê w obserwowanym obiekcie.
--- BehavioralDesignPatterns/Observer/Program.cs
+++ BehavioralDesignPatterns/Observer/Program.cs
@@ -4,7 +4,19 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var subscriber1 = new Subscriber("sub1");
+            var subscriber2 = new Subscriber("sub2");
+
+            var publisher = new Publisher();
+
+            publisher.Subscribe(subscriber1);
+            publisher.Subscribe(subscriber2);
+
+            publisher.Notify("test message");
+
+            publisher.Unsubscribe(subscriber1);
+
+            publisher.Notify("test message2");
         }
     }
 }
